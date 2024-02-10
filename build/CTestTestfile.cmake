b0dfdfc12f9f7198dd8fa6ec22e4a73551bf59b1
Aland Mulia Pratama <13522124@std.stei.itb.ac.id>
2024-02-10 15:57:49 +0700

    feat: add Autonomous Feature

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/karol/OneDrive - Institut Teknologi Bandung/Stuffs/Aksantara/Repositories/Tugas1_RSC_5
-# Build directory: C:/Users/karol/OneDrive - Institut Teknologi Bandung/Stuffs/Aksantara/Repositories/Tugas1_RSC_5/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
